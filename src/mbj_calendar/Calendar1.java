72a3f6550c4d42652268619485d3e071b5b3dc51
Metabiz <Metabiz@DESKTOP-HKE2AGF>
2022-03-20 13:58:07 +0900

    Add repeat function

--- src/mbj_calendar/Calendar1.java
+++ src/mbj_calendar/Calendar1.java
@@ -4,8 +4,10 @@ import java.util.Scanner;
 public class Calendar1 {
 	private static final int[] MAX_DAYS = {31,28,31,30,31,30,31,31,30,31,30,31};
 	public int getMaxDaysOfMonth (int month) {
+	
 	return MAX_DAYS[month-1];
 	}
+	
 	public void printSampleCalendar() {
 		System.out.println("일  월  화  수  목  금  토");
 		System.out.println("----------------------");
@@ -16,16 +18,24 @@ public class Calendar1 {
 	}
 
 	public static void main(String[] args) {
+		
+		
 
 		//숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
 		Scanner scanner = new Scanner(System.in);
 		Calendar1 cal = new Calendar1();
-		System.out.println("'달'을 입력해 주세요.");
-		int month = scanner.nextInt();
-		//print+f 에서의 입력값은 %d로 표현하고 줄바꿈은 \n으로 표시
-		System.out.println(month+"월은 "+cal.getMaxDaysOfMonth(month)+"일 까지 있습니다." );
 		
-		cal.printSampleCalendar();
+		System.out.println("반복횟수를 입력하세요.");
+		int repeat = scanner.nextInt();
+		
+		for(int i =0;i< repeat; i++) {
+			System.out.println("'달'을 입력해 주세요.");
+			int month = scanner.nextInt();
+			//print+f 에서의 입력값은 %d로 표현하고 줄바꿈은 \n으로 표시
+			System.out.println(month+"월은 "+cal.getMaxDaysOfMonth(month)+"일 까지 있습니다." );
+		}
+		System.out.println("BYE~!!");
+		//cal.printSampleCalendar();
 		
 		scanner.close();
 	}
